0b34c7c83743f83d44b9256a2399439a64af44aa
Fabio <fassil0203@gmail.com>
2024-03-14 05:29:33 -0300

    Construtores

--- Banco/src/Conta.java
+++ Banco/src/Conta.java
@@ -4,8 +4,23 @@ public class Conta {
     int numero;
     double saldo;
 
-    void depositar (double valor){
+    void depositar (double valor){      //metodo
+        if (valor <=0){
+            throw new IllegalArgumentException("Valor deve ser maior que Zero");
+        }
         saldo =saldo + valor;
+    }
+    void sacar (double valor) throws IllegalAccessException {
+        if (valor <=0){
+            throw new IllegalArgumentException("Valor deve ser maior que Zero");
+        }
+        if(saldo - valor<0){
+            throw  new IllegalAccessException("Saldo Insuficiente");
+        }
+        saldo = saldo - valor;
+    }
+    void sacar (double valor, double taxaSaque) throws IllegalAccessException {
+       sacar(valor + taxaSaque);
     }
 
 }
--- Banco/src/Main.java
+++ Banco/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws IllegalAccessException {
         Pessoa titular1 = new Pessoa();
         titular1.nome = "Fabio Santana";
         titular1.documentotitular ="12121212";
@@ -13,26 +12,31 @@ public class Main {
         minhaConta.titular = titular1;
         minhaConta.agencia = 8253-8;
         minhaConta.numero = 1696-0;
-        minhaConta.saldo = 5500.05;
+      //  minhaConta.saldo = 5500.00;
+        minhaConta.depositar(8000);
+        minhaConta.sacar(500,10);
 
         Conta suaConta = new Conta();
         suaConta.titular = titular2;
         suaConta.agencia = 8253-8;
         suaConta.numero = 1696-0;
-        suaConta.saldo = 5500.00;
+       // suaConta.saldo = 5500.00;
+        suaConta.depositar(4000.00);
 
 
         System.out.println("Titular :"+ titular2.nome);
         System.out.println("Agencia :" + minhaConta.agencia);
         System.out.println("Numero da Conta : " + minhaConta.numero);
+        System.out.println("Saldo da Conta : " + minhaConta.saldo);
 
+        System.out.println("***************************************************");
         System.out.println("***************************************************");
 
 
         System.out.println("Titula da Conta : "+ titular1.nome);
         System.out.println("Agencia :" + suaConta.agencia);
         System.out.println("Numero da Conta :" + suaConta.numero);
-        System.out.println("Seu Saldo :" + suaConta.saldo);
+        System.out.println("Saldo da Conta :" + suaConta.saldo);
 
     }
 }
