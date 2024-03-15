f1372f2d616b1aa54c5cd20067d0938cf327bf60
Fabio <fassil0203@gmail.com>
2024-03-15 05:52:09 -0300

    Encapsulamento

--- Banco/src/Conta.java
+++ Banco/src/Conta.java
@@ -1,8 +1,52 @@
+import java.util.Objects;
+
 public class Conta {
-    Pessoa titular;
-    int agencia;
-    int numero;
-    double saldo;
+    private Pessoa titular;
+    private int agencia;
+    private int numero;
+   private  double saldo;
+
+    Conta(){        //Construtor sem argumentos
+    }
+
+    public Conta(Pessoa titular, int agencia, int numero) {
+        Objects.requireNonNull(titular) ;               //tem que ter a classe titular  boa pratica
+        this.titular = titular;
+        this.agencia = agencia;
+        this.numero = numero;
+    }
+
+    public Pessoa getTitular() {
+        return titular;
+    }
+
+    public void setTitular(Pessoa titular) {
+        this.titular = titular;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 
     void depositar (double valor){      //metodo
         if (valor <=0){
@@ -10,7 +54,7 @@ public class Conta {
         }
         saldo =saldo + valor;
     }
-    void sacar (double valor) throws IllegalAccessException {
+    void sacar (double valor) throws IllegalAccessException {               //metodo
         if (valor <=0){
             throw new IllegalArgumentException("Valor deve ser maior que Zero");
         }
--- Banco/src/Main.java
+++ Banco/src/Main.java
@@ -1,27 +1,33 @@
 public class Main {
     public static void main(String[] args) throws IllegalAccessException {
         Pessoa titular1 = new Pessoa();
-        titular1.nome = "Fabio Santana";
-        titular1.documentotitular ="12121212";
+        titular1.setNome("Fabio Silva");
+        titular1.setDocumento("12121212");
 
         Pessoa titular2 =new Pessoa();
         titular2.nome = "Rafael Santana";
         titular2.documentotitular ="23232323";
 
-        Conta minhaConta = new Conta();
-        minhaConta.titular = titular1;
-        minhaConta.agencia = 8253-8;
-        minhaConta.numero = 1696-0;
+        Conta minhaConta = new Conta(titular1,8253-8,1696-0);
+        //minhaConta.titular = titular1;
+        //minhaConta.agencia = 8253-8;
+        //minhaConta.numero = 1696-0;
       //  minhaConta.saldo = 5500.00;
-        minhaConta.depositar(8000);
-        minhaConta.sacar(500,10);
+       // minhaConta.depositar(8000);
+        //minhaConta.sacar(500,10);
+        minhaConta.depositar(25000);
+        minhaConta.sacar(5000,10);
+        minhaConta.saldo = minhaConta.saldo - 45000;
 
-        Conta suaConta = new Conta();
-        suaConta.titular = titular2;
-        suaConta.agencia = 8253-8;
-        suaConta.numero = 1696-0;
+
+        Conta suaConta = new Conta(titular2,312352-7,0036);
+        //suaConta.titular = titular2;
+       // suaConta.agencia = 8253-8;
+        //suaConta.numero = 1696-0;
        // suaConta.saldo = 5500.00;
-        suaConta.depositar(4000.00);
+        suaConta.depositar(40000.00);
+        minhaConta.sacar(42000);
+
 
 
         System.out.println("Titular :"+ titular2.nome);
